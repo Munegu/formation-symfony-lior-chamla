8ac4ad6bd07dd3311f2e69b807a11ddfd28adb97
Nicolas Villetelle <nicolas.villetelle@hotmail.fr>
2020-05-04 12:22:27 +0200

    User et authentification

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,25 +9,6 @@ class HomeController extends AbstractController
 {
 
     /**
-     * Montre la page qui dit bonjour
-     *
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name="hello_base")
-     * @Route("/hello/{prenom}", name="hello_prenom")
-     *
-     */
-    public function hello($prenom = "anonyme", $age = 0){
-        return $this->render( 'hello.html.twig',
-            ['prenom'=>$prenom,
-                'age'=>$age
-            ]
-        );
-    }
-
-
-
-
-    /**
      * @Route("/", name="homepage")
      */
     public function home()
