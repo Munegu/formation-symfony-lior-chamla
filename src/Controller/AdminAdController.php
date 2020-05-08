630de0fde4f895e0362d51d2d694ee187b61b263
Nicolas Villetelle <nicolas.villetelle@hotmail.fr>
2020-05-08 12:51:09 +0200

    Gestion partie pagination

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use App\Repository\AdRepository;
+use App\Service\PaginationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminAdController extends AbstractController
 {
     /**
-     * @Route("/admin/ads", name="admin_ads_index")
+     * @Route("/admin/ads/{page}", name="admin_ads_index", requirements={"page": "\d+"})
      */
-    public function index(AdRepository $repo)
+    public function index(AdRepository $repo, $page = 1, PaginationService $pagination)
     {
+//        $limit = 10;
+//
+//        $start = $page * $limit - $limit;
+//        // 1 * 10 = 10 - 10 = 0
+//        // 2 * 10 = 20 - 10 = 10
+//
+//        $total = count($repo->findAll());
+//        $pages = ceil($total / $limit );
+
+        $pagination->setEntityClass(Ad::class);
+        $pagination->setCurrentPage($page);
+
         return $this->render('admin/ad/index.html.twig', [
-            'ads'=>$repo->findAll()
+            'pagination'=>$pagination
         ]);
     }
 }
